46d91f03eeee9b11c9f33ce138f4a270a3da18f4
Thomas_Hinterberger <[hinterberger.thomas@sus.htl-grieskirchen.at]>
2021-02-01 10:04:45 +0100

    optimised imports and beautified code

--- app/src/main/java/com/example/a_gps_c_uebung/Gps_show_data.java
+++ app/src/main/java/com/example/a_gps_c_uebung/Gps_show_data.java
@@ -1,12 +1,12 @@
 package com.example.a_gps_c_uebung;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.a_gps_c_uebung.database.GPSTbl;
 import com.example.a_gps_c_uebung.database.GPS_DB_Helper;
@@ -15,19 +15,17 @@ import java.util.ArrayList;
 
 public class Gps_show_data extends AppCompatActivity {
 
-    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps_show_data);
-        GPS_DB_Helper db_helper = new GPS_DB_Helper(this);
-        db = db_helper.getReadableDatabase();
-
         loadData();
     }
 
     private void loadData() {
+        GPS_DB_Helper db_helper = new GPS_DB_Helper(this);
+        SQLiteDatabase db = db_helper.getReadableDatabase();
         Cursor rows = db.rawQuery(GPSTbl.GPSSTMT_SELECT, null);
         ArrayList<String> arrayList = new ArrayList<>();
 
@@ -35,10 +33,10 @@ public class Gps_show_data extends AppCompatActivity {
             String longitude = rows.getString(1);
             String latitude = rows.getString(2);
             String dateTime = rows.getString(3);
-            arrayList.add("longitude= "+longitude+" \nlatitude="+latitude+" \nmyDate ="+dateTime);
+            arrayList.add("longitude= " + longitude + " latitude=" + latitude + " myDate =" + dateTime);
         }
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,arrayList);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, arrayList);
         ListView listView = findViewById(R.id.listViewGpsData);
         listView.setAdapter(arrayAdapter);
     }
